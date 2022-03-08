dea68ecd54685af04ed9a7f0645a83119baf7535
mraheeldev <m.raheeldev@gmail.com>
2022-03-08 15:21:37 +0500

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/raheel/com/fragments/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package raheel.com.fragments;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "raheel.com.fragments";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/raheel/com/fragments/fragments/MainActivity.java
@@ -0,0 +1,21 @@
+package raheel.com.fragments.fragments;
+
+import android.os.Bundle;
+
+
+import androidx.fragment.app.FragmentActivity;
+
+import raheel.com.fragments.R;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
